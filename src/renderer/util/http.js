bdb37b3d35256f381f51a31345d07f5a04fa6423
Guilherme Ferreira <gmferreir1@gmail.com>
2019-05-29 11:17:22 -0300

    Alterando a porta da API

--- src/renderer/util/http.js
+++ src/renderer/util/http.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 //const url = window.location.host.split(":")[0];
 
 const urlDevelopment = "http://localhost:3333";
-const urlProduction = "http://10.0.10.95:9091";
+const urlProduction = "http://10.0.10.95:7070";
 
 window.URL_API = process.env.NODE_ENV === 'development' ? urlDevelopment : urlProduction
 
